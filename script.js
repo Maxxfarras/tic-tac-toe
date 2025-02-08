d6232ba57af85d809a90f3b3cbaa4ea30e5e63c3
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2025-02-07 22:27:12 -0700

    change completely the gameboard object add rows and individual cells
    new function tiles()
    changes the values for each tile, returns the tile
    value.

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 gameboard = (function () {
-    row1 = [0,1,2];
-    row2 = [0,1,2];
-    row3 = [0,1,2];
-    return {row1, row2, row3}
+  rows = 3;
+  columns = 3;
+  board = [];
+  for (i = 0; i < rows; i++) {
+    board[i] = [];
+    for (j = 0; j < columns; j++) {
+      board[i].push(tile());
+    }
+  }
+  const getBoard = () => board;
+  return { board, getBoard };
 })();
 
 function player(name, mark) {
-    let personalScore = 0;
-    const getPersonalScore = () => personalScore;
-    const addPersonalScore = () => personalScore += 1;
-    return {name, mark, getPersonalScore, addPersonalScore}
+  let personalScore = 0;
+  const getPersonalScore = () => personalScore;
+  const addPersonalScore = () => (personalScore += 1);
+  return { name, mark, getPersonalScore, addPersonalScore };
+}
+
+function tile() {
+  let value = 0;
+  const getValue = () => value;
+  const changeValue = (mark) => (value = mark);
+  return {
+    getValue,
+    changeValue,
+  };
 }
