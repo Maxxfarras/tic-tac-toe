71d6ab1853d483b29f958fa185384ae5c4831c32
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2025-02-07 21:57:00 -0700

    add basic factories for gameboard and player prone to change

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+gameboard = (function () {
+    row1 = [0,1,2];
+    row2 = [0,1,2];
+    row3 = [0,1,2];
+    return {row1, row2, row3}
+})();
+
+function player(name, mark) {
+    let personalScore = 0;
+    const getPersonalScore = () => personalScore;
+    const addPersonalScore = () => personalScore += 1;
+    return {name, mark, getPersonalScore, addPersonalScore}
+}
