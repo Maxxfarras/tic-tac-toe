f00fc8da02bd7f571c7858c106f9672117d74d8b
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2025-02-09 22:30:44 -0700

    change to a "cleaner" syntax on checkerBoard() add break to the for loops to avoid redundancy use ternary operator on threeInCross cuz why not

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ gameboard = (function () {
   }
 */
   let board = [['x','o','o'],
-               ['o','x','x'],
-               ['x','o','o']]  
+               ['x','o','x'],
+               ['o','p','o']]  
   const getBoard = () => board;
   const addMark = (row, column, mark) => {
     if (board[row][column] === undefined) {
@@ -30,22 +30,31 @@ gameboard = (function () {
     let threeInRow = false;
     for (i = 0; i < board.length; i++) {
       for (j = 0; j < board[i].length - 2; j++) {
-        board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2] ? threeInRow = true: false
+        if (board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2]) {
+          threeInRow = true;
+          break
+        } else {
+          continue
+        }
       }
     }
+
     let threeInColumn = false;
     for (i = 0; i < board.length; i++) {
       for (j = 0; j < board[j].length - 2; j++) {
-        board[j][i] == board[j + 1][i] && board[j][i] == board[j + 2][i] ? threeInColumn = true : false
+        if (board[j][i] == board[j + 1][i] && board[j][i] == board[j + 2][i]) {
+          threeInColumn = true;
+          break;
+        } else {
+          continue
+        }
       }
     }
+
     let threeInCross = false;
-    if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) { //this code is trash but works
-      threeInCross = true;
-    }
-    if (board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
-      threeInCross = true;
-    }
+    board[0][0] == board[1][1] && board[0][0] == board[2][2] ? threeInCross = true : false;
+    board[0][2] == board[1][1] && board[0][2] == board[2][0] ? threeInCross = true : false;
+    
     if (threeInRow || threeInColumn || threeInCross) {
         return 'The game has ended'
     }
