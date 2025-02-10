7a27ae82bef9acb303bbdb763f04dff8cd12413d
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2025-02-09 21:36:31 -0700

    comment out the board add a dummy board for now will be eliminated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 gameboard = (function () {
   rows = 3;
   columns = 3;
+/*
   board = [];
   for (i = 0; i < rows; i++) {
     board[i] = [];
@@ -9,6 +10,10 @@ gameboard = (function () {
       board[i].push(tile());
     }
   }
+*/
+  let board = [['x','x','o'],
+               ['o','x','o'],
+               ['o','x','o']]  
   const getBoard = () => board;
   const addMark = (row, column, mark) => {
     if (board[row][column] === undefined) {
@@ -28,19 +33,11 @@ gameboard = (function () {
         board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2] ? threeInRow = true : false
       }
     }
+    console.log(threeInRow)
   }
   return { getBoard, addMark, printBoard, checkerBoard };
 })();
 
-gameboard.addMark(0,0,'x')
-gameboard.addMark(0,1,'o')
-gameboard.addMark(0,2,'x')
-gameboard.addMark(1,0,'x')
-gameboard.addMark(1,1,'o')
-gameboard.addMark(1,2,'x')
-gameboard.addMark(2,0,'x')
-gameboard.addMark(2,1,'o')
-gameboard.addMark(2,2,'x')
 gameboard.checkerBoard()
 
 function player(name, mark) {
