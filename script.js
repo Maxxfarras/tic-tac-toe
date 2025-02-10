d86c803d0654eee4a1677914f3dc3225cb2963dd
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2025-02-09 21:53:44 -0700

    add threeInColumn validation seems to work just fine

--- script.js
+++ script.js
@@ -33,7 +33,12 @@ gameboard = (function () {
         board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2] ? threeInRow = true : false
       }
     }
-    console.log(threeInRow)
+    let threeInColumn = false;
+    for (i = 0; i < board.length; i++) {
+      for (j = 0; j < board[j].length - 2; j++) {
+        board[j][i] == board[j + 1][i] && board[j][i] == board[j + 2][i] ? threeInColumn = true : false
+      }
+    }
   }
   return { getBoard, addMark, printBoard, checkerBoard };
 })();
