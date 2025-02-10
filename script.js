b1af66824d9e4a5406d9514adf8ffdc79126bcc7
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2025-02-09 21:32:36 -0700

    add checkerBoard() is a set of booleans to check if there is a match only works on the threeInRow

--- script.js
+++ script.js
@@ -21,8 +21,27 @@ gameboard = (function () {
     const boardWithValues = board.map((row) => row.map((tile) => tile));
     console.log(boardWithValues);
   };
-  return { getBoard, addMark, printBoard };
+  const checkerBoard = () => {
+    let threeInRow = false;
+    for (i = 0; i < board.length; i++) {
+      for (j = 0; j < board[i].length - 2; j++) {
+        board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2] ? threeInRow = true : false
+      }
+    }
+  }
+  return { getBoard, addMark, printBoard, checkerBoard };
 })();
+
+gameboard.addMark(0,0,'x')
+gameboard.addMark(0,1,'o')
+gameboard.addMark(0,2,'x')
+gameboard.addMark(1,0,'x')
+gameboard.addMark(1,1,'o')
+gameboard.addMark(1,2,'x')
+gameboard.addMark(2,0,'x')
+gameboard.addMark(2,1,'o')
+gameboard.addMark(2,2,'x')
+gameboard.checkerBoard()
 
 function player(name, mark) {
   let personalScore = 0;
