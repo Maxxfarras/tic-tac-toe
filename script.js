80e34b12ca5d671a54a4822adb45690ca84d5428
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2025-02-09 22:33:34 -0700

    format document

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 gameboard = (function () {
   rows = 3;
   columns = 3;
-/*
+  /*
   board = [];
   for (i = 0; i < rows; i++) {
     board[i] = [];
@@ -11,10 +11,14 @@ gameboard = (function () {
     }
   }
 */
-  let board = [['x','o','o'],
-               ['x','o','x'],
-               ['o','p','o']]  
+  let board = [
+    ["x", "o", "o"],
+    ["x", "o", "x"],
+    ["o", "p", "o"],
+  ];
+
   const getBoard = () => board;
+
   const addMark = (row, column, mark) => {
     if (board[row][column] === undefined) {
       board[row][column] = mark;
@@ -22,19 +26,21 @@ gameboard = (function () {
       return;
     }
   };
+
   const printBoard = () => {
     const boardWithValues = board.map((row) => row.map((tile) => tile));
     console.log(boardWithValues);
   };
+
   const checkerBoard = () => {
     let threeInRow = false;
     for (i = 0; i < board.length; i++) {
       for (j = 0; j < board[i].length - 2; j++) {
         if (board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2]) {
           threeInRow = true;
-          break
+          break;
         } else {
-          continue
+          continue;
         }
       }
     }
@@ -46,23 +52,26 @@ gameboard = (function () {
           threeInColumn = true;
           break;
         } else {
-          continue
+          continue;
         }
       }
     }
 
     let threeInCross = false;
-    board[0][0] == board[1][1] && board[0][0] == board[2][2] ? threeInCross = true : false;
-    board[0][2] == board[1][1] && board[0][2] == board[2][0] ? threeInCross = true : false;
-    
+    board[0][0] == board[1][1] && board[0][0] == board[2][2]
+      ? (threeInCross = true)
+      : false;
+    board[0][2] == board[1][1] && board[0][2] == board[2][0]
+      ? (threeInCross = true)
+      : false;
+
     if (threeInRow || threeInColumn || threeInCross) {
-        return 'The game has ended'
+      return "The game has ended";
     }
-  }
+  };
+
   return { getBoard, addMark, printBoard, checkerBoard };
 })();
-
-gameboard.checkerBoard()
 
 function player(name, mark) {
   let personalScore = 0;
